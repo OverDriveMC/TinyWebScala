a2411dbd933a4f27691985a3d87f9ae30c864c60
OverDriveMC <mengcheng1994@gmail.com>
2015-10-02 15:49:44 +0800

    view

--- src/com/mblinn/mpfpp/oo/tinyweb/stepone/FunctionView.scala
+++ src/com/mblinn/mpfpp/oo/tinyweb/stepone/FunctionView.scala
@@ -6,11 +6,16 @@ trait View{
 class FunctionView(
     viewRenderer :(Map[String,List[String]])=>String) 
                         extends View {
+  
+  /**
+   * 该方法以model为入参，并将该model传入viewRenderer方法运行
+   */
   def render(model: Map[String, List[String]]): String = {
     try
       viewRenderer(model)
      catch{
-       case e:Exception => throw new RenderingException(e)  
+       case e:Exception => throw new RenderingException(e)
      }     
   }
 }
+
